9ebf7e178a1bb0f788a8fb74def9416b434d82c8
eswaryalakanti <ylakshmichandra03@gmail.com>
2024-09-13 00:09:10 +0530

    changed frontend url

--- Server/app.js
+++ Server/app.js
@@ -27,7 +27,7 @@ const app=express();
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-    origin:`${process.env.FROTEND_URL}`,
+    origin:"https://learning-management-system-ten-flame.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials:true,
     optionsSuccessStatus: 200,
