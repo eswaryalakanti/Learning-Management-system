d38e913ffde95ef08f712294e57fff0be21a8b89
eswaryalakanti <ylakshmichandra03@gmail.com>
2024-09-13 15:35:52 +0530

    video commit newf new

--- client/lms-frontend/src/redux/slice/lectureSlice.js
+++ client/lms-frontend/src/redux/slice/lectureSlice.js
@@ -61,7 +61,7 @@ export const addlecture=createAsyncThunk('/course/lectures/create',async({cid,da
 
 })
 
-export const deletelecture=createAsyncThunk('/course/lectures/create',async(cid,lid)=>{
+export const deletelecture=createAsyncThunk('/course/lectures/create',async({cid:cid,lid:lid})=>{
   
     try {
         const response=await axiosInstance.post(`courses?courseId=${cid} && lectureId=${lid}`,formData);
